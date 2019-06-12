4837aac83fc3fef529005c8ca7aaa281cea8e541
Grégoire Roumache <groumache@gmail.com>
2019-06-12 10:32:26 +0200

    version2 p001

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,11 @@ pub mod problem1to10;
 // If we list all the natural numbers below 10 that are multiples
 // of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #[cfg(test)]
-mod p001 {
-    use crate::problem1to10::prob1;
+mod tests {
+    use crate::problem1to10::p001;
     #[test]
     fn test_prob_1() {
-        assert_eq!(prob1::version1(10), 23);
+        assert_eq!(p001::version1(10), 23);
+        assert_eq!(p001::version2(10), 23);
     }
 }
--- src/problem1to10.rs
+++ src/problem1to10.rs
@@ -1,9 +1,18 @@
 // Problem 1: Multiples of 3 and 5
 //  Find the sum of all the multiples of 3 or 5 below 1000.
-pub mod prob1 {
-    pub fn version1(limit: i32) -> i32 {
+pub mod p001 {
+    pub fn version1(limit: i32) -> i32 {    // O(n)
         let sum3: i32 = (3..limit).step_by(3).sum();
         let sum5: i32 = (5..limit).filter(|x| x % 3 != 0).step_by(5).sum();
         sum3 + sum5
+    }
+    pub fn version2(limit: i32) -> i32 {    // O(1)
+        let max3 = (limit - 1) / 3;
+        let max5 = (limit - 1) / 5;
+        let max3x5 = limit / (3 * 5);
+        let sum3 = 3 * max3 * (1 + max3) / 2; 
+        let sum5 = 5 * max5 * (1 + max5) / 2;
+        let sum3x5 = (3 * 5) * max3x5 * (1 + max3x5) / 2;
+        sum3 + sum5 - sum3x5
     }
 }
